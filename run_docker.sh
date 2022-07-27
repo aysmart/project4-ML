179d90cd72e7b50ebc7cc24685e5fcc241e38b26
Ayorinde Smart <smartayorinde@gmail.com>
2022-07-27 11:01:56 +0000

    fix: docker files

--- run_docker.sh
+++ run_docker.sh
@@ -4,10 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t udacity/project4:1.0 .
+docker build -t udacity-project4 .
 # Step 2: 
 # List docker images
-docker images
+docker images ls
 # Step 3: 
 # Run flask app
-docker run -d -p 80:80 udacity/project4:1.0
+docker run -d -p 8080:80 udacity-project4
